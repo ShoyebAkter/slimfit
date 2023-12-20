2bd0b4bc6733c8aef0e51bbe0312e43f1e8509a0
Shoyeb Akter <heroreal5385@gmail.com>
2023-12-20 15:28:15 +0600

    changing css

---
+++ scripts/login.js
@@ -0,0 +1,22 @@
+function showErrorToast(error) {
+    // Create a new toast element
+    const toast = document.createElement("div");
+    toast.className = "toast";
+    toast.innerHTML = error;
+  
+    // Append the toast to the container
+    document.getElementById("error-toast-container").appendChild(toast);
+  
+    // Show the toast
+    setTimeout(function () {
+      toast.style.display = "block";
+    }, 1000);
+  
+    // Hide the toast after 3 seconds
+    setTimeout(function () {
+      toast.style.display = "none";
+      // Remove the toast element from the DOM after it's hidden
+      document.getElementById("payment-toast-container").removeChild(toast);
+      window.location.href = "menu.html";
+    }, 4000);
+  }
